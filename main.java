0d7d7f9b9fc4f9161c4ab0593628f3e106cb455f
bemengseang015-del <bemengseang015@gmail.com>
2026-03-23 15:59:37 +0700

    First commit - Java typing master

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("------------ Welcome to I3 Typing Master v2026 -----------");
+        System.out.println("To begin, please select one of the following options:");
+        System.out.println("  1. Login");
+        System.out.println("  2. Register");
+        System.out.println("  3. Exit");
+        System.out.print("Choose an option: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You selected Login.");
+                break;
+            case 2:
+                System.out.println("You selected Register.");
+                break;
+            case 3:
+                System.out.println("Exiting... Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+
+        scanner.close();
+    }
+}
